9d2260457cda12a6735830ccfb4d7e5cb1abd33e
Nicolas Daniel Palumbo <n@xinax.net>
2014-06-06 12:17:14 +0200

    Added Facade and base class

---
+++ src/Xinax/LaravelGettext/LaravelGettext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Xinax\LaravelGettext;
+
+class LaravelGettext{
+
+	public static function checkGettextExtension(){
+		echo "alooooo";
+	}
+
+}
--- src/Xinax/LaravelGettext/LaravelGettextServiceProvider.php
+++ src/Xinax/LaravelGettext/LaravelGettextServiceProvider.php
@@ -2,43 +2,45 @@
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Laravel gettext main service provider
+ */
 class LaravelGettextServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
-	 *
 	 * @var bool
 	 */
 	protected $defer = false;
 
 	/**
 	 * Bootstrap the application events.
-	 *
 	 * @return void
 	 */
-	public function boot()
-	{
+	public function boot(){
 		$this->package('xinax/laravel-gettext');
 	}
 
 	/**
 	 * Register the service provider.
-	 *
 	 * @return void
 	 */
-	public function register()
-	{
-		//
+	public function register(){
+		$this->app['laravel-gettext'] = $this->app->share(function($app){
+			return new LaravelGettext;
+		});
+		$this->app->booting(function(){
+			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias('LaravelGettext', 'Xinax\LaravelGettext\Facades\LaravelGettext');
+		});
 	}
 
 	/**
 	 * Get the services provided by the provider.
-	 *
 	 * @return array
 	 */
-	public function provides()
-	{
-		return array();
+	public function provides(){
+		return array('laravel-gettext');
 	}
 
 }
