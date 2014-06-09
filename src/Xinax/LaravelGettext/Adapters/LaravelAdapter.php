4700d396a98806bdfb2246fc1c4210f2f4214143
Nicolas Daniel Palumbo <n@xinax.net>
2014-06-09 20:18:41 +0200

    More testing

---
+++ src/Xinax/LaravelGettext/Adapters/LaravelAdapter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Xinax\LaravelGettext\Adapters;
+
+class LaravelAdapter implements AdapterInterface{
+
+	/**
+	 * Returns the adapter current locale
+	 */
+	public function setLocale($locale){
+		\App::setLocale(substr($locale, 0, 2)); 
+	}
+
+	/**
+	 * Sets the locale on current addapter
+	 */
+	public function getLocale(){
+		return \App::getLocale();
+	}
+
+	/**
+	 * Return the application path
+	 */
+	public function getApplicationPath(){
+		return app_path();
+	}
+
+}
