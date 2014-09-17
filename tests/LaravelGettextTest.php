d2189885194e53007d3adfaf06cbd8bbd2dacfd7
matiit <matiqing@gmail.com>
2014-09-17 19:33:37 +0100

    formatting and docblocks

--- tests/LaravelGettextTest.php
+++ tests/LaravelGettextTest.php
@@ -1,52 +1,83 @@
 <?php
 
 namespace Xinax\LaravelGettext\Test;
+
 use \Mockery as m;
 use \Xinax\LaravelGettext\LaravelGettext;
 
-class LaravelGettextTest extends \PHPUnit_Framework_TestCase {
+class LaravelGettextTest extends \PHPUnit_Framework_TestCase
+{
 
-	protected $laravelGettext;
+    /**
+     * @var LaravelGettext
+     */
+    protected $laravelGettext;
 
-	public function setUp(){
+    /**
+     * @inheritdoc
+     */
+    public function setUp()
+    {
 
-		$gettext = m::mock('Xinax\LaravelGettext\Gettext');
-		
-		$gettext->shouldReceive('getEncoding')->andReturn('UTF-8');
-		$gettext->shouldReceive('setEncoding')->with('UTF-8');
-		$gettext->shouldReceive('getLocale')->andReturn('en_US');
-		$gettext->shouldReceive('setLocale')->with('en_US');
-		$gettext->shouldReceive('filesystemStructure')->andReturn(true);
+        $gettext = m::mock('Xinax\LaravelGettext\Gettext');
 
-		$this->laravelGettext = new LaravelGettext($gettext);
+        $gettext->shouldReceive('getEncoding')->andReturn('UTF-8');
+        $gettext->shouldReceive('setEncoding')->with('UTF-8');
+        $gettext->shouldReceive('getLocale')->andReturn('en_US');
+        $gettext->shouldReceive('setLocale')->with('en_US');
+        $gettext->shouldReceive('filesystemStructure')->andReturn(true);
 
-		parent::setUp();
+        $this->laravelGettext = new LaravelGettext($gettext);
 
-	}
+        parent::setUp();
 
-	public function tearDown(){
-		m::close();
-	}
+    }
 
-	public function testLaravelGettextInstance(){
-		$this->assertInstanceOf('Xinax\LaravelGettext\LaravelGettext', $this->laravelGettext);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
 
-	public function testGetEncoding(){
-		$this->assertEquals('UTF-8', $this->laravelGettext->getEncoding());
-	}
+    /**
+     * Test setting an instance.
+     */
+    public function testLaravelGettextInstance()
+    {
+        $this->assertInstanceOf('Xinax\LaravelGettext\LaravelGettext', $this->laravelGettext);
+    }
 
-	public function testSetEncoding(){
-		$this->laravelGettext->setEncoding('UTF-8');
-	}
+    /**
+     * Test getEncoding() method.
+     */
+    public function testGetEncoding()
+    {
+        $this->assertEquals('UTF-8', $this->laravelGettext->getEncoding());
+    }
 
-	public function testSetLocale(){
-		$this->laravelGettext->setLocale('en_US');
-	}
+    /**
+     * Test setEncoding() method.
+     */
+    public function testSetEncoding()
+    {
+        $this->laravelGettext->setEncoding('UTF-8');
+    }
 
-	public function testGetLocale(){
-		$this->assertEquals('en_US', $this->laravelGettext->getLocale());
-	}
+    /**
+     * Test setLocale() method.
+     */
+    public function testSetLocale()
+    {
+        $this->laravelGettext->setLocale('en_US');
+    }
 
-
+    /**
+     * Test getLocale() method.
+     */
+    public function testGetLocale()
+    {
+        $this->assertEquals('en_US', $this->laravelGettext->getLocale());
+    }
 }
