ef537fc63ea0211726527c3147710b446c013a9e
Nicolas Daniel Palumbo <npalumbo@xinax.net>
2014-10-08 12:45:29 +0200

    - Filter explained

--- src/filters.php
+++ src/filters.php
@@ -2,5 +2,11 @@
 
 App::before(function($request)
 {
+	/**
+	 * The package need to be initialize, the locale will 
+	 * be available from the first method call. If you have 
+	 * async calls in your project, this filter starts the 
+	 * locale environment before each request.
+	 */
     LaravelGettext::getLocale();
 });
