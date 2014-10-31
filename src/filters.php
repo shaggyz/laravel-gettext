0356fe3c94ad4ba8f54810b3a531ebaa5c09158d
Nicolas Daniel Palumbo <n@xinax.net>
2014-10-31 11:57:13 +0100

    - Deleted empty directories and deprecated files

--- src/filters.php
+++ src/filters.php
@@ -3,8 +3,8 @@
 App::before(function($request)
 {
 	/**
-	 * The package need to be initialize, the locale will 
-	 * be available from the first method call. If you have 
+	 * The package need to be initialized, the locale will
+	 * be available after first method call. If you have
 	 * async calls in your project, this filter starts the 
 	 * locale environment before each request.
 	 */
