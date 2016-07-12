61a6522cbcff474c028883c7de293937ce7e03f6
Charles Rumley <crumley@johnson-rauhoff.com>
2016-07-11 23:03:55 -0400

    Formatting improvements

--- tests/unit/BaseTestCase.php
+++ tests/unit/BaseTestCase.php
@@ -2,8 +2,10 @@
 
 namespace Xinax\LaravelGettext\Test;
 
-use \Mockery as m;
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
 
-abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
+abstract class BaseTestCase extends PHPUnit_Framework_TestCase
 {
+
 }
